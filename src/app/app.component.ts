1a9a0ed8e382b2f2829c2b7565d69300e2fd260f
Michal Rozanski <michal.rozanski@altkomsoftware.pl>
2020-05-08 15:36:42 +0200

    Second lesson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Quotation } from './models/quotations';
+import { QUOTES } from './models/database';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'best-quotes-app';
+
+  showForm = false;
+  quotes: Quotation[] = QUOTES;
+  quotation: Quotation = { author: '', quotation: '', votes: 0 };
+
+  onSwitchForm(): void {
+    this.showForm = !this.showForm;
+  }
+
+  addQuotation() {
+    this.quotes.unshift(this.quotation);
+    this.quotation =  { author: '', quotation: '', votes: 0 };
+  }
+
+  addVote(quotation: Quotation, value: number) {
+    quotation.votes += value;
+  }
+
 }
